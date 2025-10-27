bf026edee702ef09dbfa528323853fddc30f3fd0
Peter Ilberg <peter.ilberg@gmail.com>
2025-10-27 20:29:43 +0100

    Move state machines into separate module.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
-use std::fmt::Display;
+mod state_machine {
+    use std::fmt::Display;
 
-trait StateMachine<S, I, O> {
-    fn get_start_state(&self) -> S;
-    fn get_next_state(&self, state: S, input: &I) -> (S, O);
+    pub trait StateMachine<S, I, O> {
+        fn get_start_state(&self) -> S;
+        fn get_next_state(&self, state: S, input: &I) -> (S, O);
+    }
 
-    fn run(&self, inputs: &[I]) -> Vec<O> {
+    pub fn run<S, I, O>(
+        state_machine: &dyn StateMachine<S, I, O>,
+        inputs: &[I],
+    ) -> Vec<O> {
         let mut outputs = Vec::new();
-        self.transduce(
+        transduce(
+            state_machine,
             inputs,
             |_state| {},
             |_i, _input, output, _state| {
@@ -16,44 +22,56 @@ trait StateMachine<S, I, O> {
         outputs
     }
 
-    fn trace(&self, inputs: &[I]) -> Vec<O>
+    pub fn trace<S, I, O>(
+        state_machine: &dyn StateMachine<S, I, O>,
+        inputs: &[I],
+    ) -> Vec<O>
     where
         S: Display,
         I: Display,
         O: Display,
     {
         let mut outputs = Vec::new();
-        self.transduce(
+        transduce(
+            state_machine,
             inputs,
             |state| {
                 println!("Start state: {state}");
             },
             |i, input, output, state| {
-                println!("{i}: input {input} produces {output} with new state: {state}");
+                print!("{i}: input {input} produces {output}");
+                println!(" with new state: {state}");
                 outputs.push(output);
             },
         );
         outputs
     }
 
-    fn transduce<FS, FN>(&self, inputs: &[I], mut start_state_fn: FS, mut next_state_fn: FN)
-    where
+    fn transduce<S, I, O, FS, FN>(
+        state_machine: &dyn StateMachine<S, I, O>,
+        inputs: &[I],
+        mut start_state_fn: FS,
+        mut next_state_fn: FN,
+    ) where
         FS: FnMut(&S),
         FN: FnMut(usize, &I, O, &S),
     {
-        let start_state = self.get_start_state();
+        let start_state = state_machine.get_start_state();
         start_state_fn(&start_state);
 
         inputs
             .iter()
             .enumerate()
             .fold(start_state, |state, (i, input)| {
-                let (next_state, output) = self.get_next_state(state, input);
+                let (next_state, output) =
+                    state_machine.get_next_state(state, input);
                 next_state_fn(i, input, output, &next_state);
                 next_state
             });
     }
 }
+
+use state_machine::{StateMachine, trace};
 
 struct Accumulator<T> {
     start_state: T,
@@ -71,14 +89,6 @@ impl StateMachine<i32, i32, i32> for Accumulator<i32> {
 
 fn main() {
     let accumulator = Accumulator { start_state: 0 };
-    let output = accumulator.trace(&[100, -3, 4, -123, 10]);
+    let output = trace(&accumulator, &[100, -3, 4, -123, 10]);
     println!("Output: {output:?}");
 }
-
-// Start state: 0
-// In: 100 Out: 100 Next State: 100
-// In: -3 Out: 97 Next State: 97
-// In: 4 Out: 101 Next State: 101
-// In: -123 Out: -22 Next State: -22
-// In: 10 Out: -12 Next State: -12
-// [100, 97, 101, -22, -12]
