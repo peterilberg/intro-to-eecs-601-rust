be984745757da888093c1b0a16c60af32f45631b
Peter Ilberg <peter.ilberg@gmail.com>
2025-10-31 19:01:53 +0100

    Rewrite state machine functions to return iterators.

--- examples/accumulator.rs
+++ examples/accumulator.rs
@@ -3,6 +3,6 @@ use intro_to_ee_and_cs::state_machines::Accumulator;
 
 fn main() {
     let accumulator = Accumulator::new(0);
-    let output = trace(&accumulator, &[100, -3, 4, -123, 10]);
+    let output: Vec<_> = trace(&accumulator, &[100, -3, 4, -123, 10]).collect();
     println!("Output: {output:?}");
 }
